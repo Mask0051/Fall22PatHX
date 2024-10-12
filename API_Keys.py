cf16d71cb9abdc782e96964b60b929b6c5b15284
Mask0051 <94188449+Mask0051@users.noreply.github.com>
2024-10-12 04:24:17 -0400

    setting up API file.

---
+++ API_Keys.py
@@ -0,0 +1,10 @@
+'''
+This file will hold the API key
+follow these steps
+
+1. replace FILLER with your API key
+2. in working python file: import API_KEY from API_Keys
+3. use API_KEY instead of hardcoding API key
+
+'''
+API_KEY = "FILLER"
