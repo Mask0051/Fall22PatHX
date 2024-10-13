b29d2868907c785d41378cf7df457b5b45de9fb4
Mask0051 <94188449+Mask0051@users.noreply.github.com>
2024-10-12 20:45:41 -0400

    connecting front to back. test1

--- GEM/src/App.jsx
+++ GEM/src/App.jsx
@@ -5,18 +5,19 @@ import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [msg, setInput] = useState(''); // Renamed setinput to setInput for clarity
+  const [msg, setInput] = useState(''); 
 
-  const submit = async (e) => { // Declare the submit function correctly
-    e.preventDefault(); // Correctly prevent the default form submission behavior
+  const submit = async (e) => { 
+    e.preventDefault(); 
 
     try {
+      alert("submitted");
       await axios.post("http://localhost:5002/", {
         msg
       });
-      setInput(''); // Clear the textarea after submission
+      setInput(''); 
     } catch (error) {
-      console.log(error); // Log any errors
+      console.log(error); 
     }
   }
 
@@ -29,15 +30,15 @@ function App() {
             <div id='chatbox'>
               <p>What do you want to practice?</p>
             </div>
-            <form onSubmit={submit}> {/* Use onSubmit for the form */}
+            <form onSubmit={submit}> 
               <textarea
                 id="inputBox"
                 name="text"
-                value={msg} // Link the textarea to the state
-                onChange={(e) => setInput(e.target.value)} // Update state on change
+                value={msg} 
+                onChange={(e) => setInput(e.target.value)} 
                 placeholder='What do you want to practice?'
               ></textarea>
-              <input type="submit" value="Submit" /> {/* Corrected value to a string */}
+              <input type="submit" value="Submit" /> 
             </form>            
           </div>
         </div>
